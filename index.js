aed12fce872772e804699e7f9cd45bc3a1d6bf29
johnny <johnny1194@hotmail.com>
2021-04-20 17:02:28 -0400

    append card

--- index.js
+++ index.js
@@ -14,4 +14,15 @@ function createCard(quotesObj){
     const quoteContainer=document.getElementById('quotes-container')
     const card = document.createElement('card')
 
+    const characterName=document.createElement('h3');
+    const quote=document.createElement('h3');
+    const animeName=document.createElement('h3');
+
+    characterName.innerHTML=`${quotesObj.character}`;
+    quote.innerHTML=`${quotesObj.quote}`;
+    animeName.innerHTML=`${quotesObj.anime}`;
+
+    quoteContainer.appendChild(card);
+    card.append(characterName,quote,animeName);
+    
 }
